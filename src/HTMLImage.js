759c3194c1a7e6438d16c820334fd41fc3d8929c
Max Rondon <mrondon@talpor.com>
2020-02-27 15:14:48 -0300

    Fixed bug not updating the image size

--- src/HTMLImage.js
+++ src/HTMLImage.js
@@ -2,13 +2,27 @@ import React, { PureComponent } from 'react';
 import { Image, View, Text } from 'react-native';
 import PropTypes from 'prop-types';
 
+
+const getDimensionsFromStyle = (style, defaultWidth, defaultHeight) => {
+    const parsedStypes = Array.isArray(style) ? style[0] : style;
+
+    const rawWidth = parsedStypes.width || defaultWidth || 0;
+    const rawHeight = parsedStypes.height || defaultHeight || 0;
+
+    const width = typeof rawWidth === 'string' && rawWidth.search('%') !== -1 ? rawWidth : parseInt(rawWidth, 10);
+    const height = typeof rawHeight === 'string' && rawHeight.search('%') !== -1 ? rawHeight : parseInt(rawHeight, 10);
+
+    return { width, height };
+}
+
 export default class HTMLImage extends PureComponent {
     constructor (props) {
         super(props);
-        this.state = {
-            width: props.imagesInitialDimensions.width,
-            height: props.imagesInitialDimensions.height
-        };
+        this.state = getDimensionsFromStyle(
+            props.style,
+            props.width || props.imagesInitialDimensions.width,
+            props.height || props.imagesInitialDimensions.height
+        );
     }
 
     static propTypes = {
@@ -31,98 +45,62 @@ export default class HTMLImage extends PureComponent {
         }
     }
 
-    componentDidMount () {
-        this.getImageSize();
-        this.mounted = true;
+    getImageSize() {
+        const { source, imagesMaxWidth, style, height, width } = this.props;
+        const dimensions = getDimensionsFromStyle(
+            props.style,
+            props.width || props.imagesInitialDimensions.width,
+            props.height || props.imagesInitialDimensions.height
+        )
+        if (dimensions.width && dimensions.height) {
+            this.setState(dimensions);
+        } else {
+            // Fetch image dimensions only if they aren't supplied or if with or height is missing
+            Image.getSize(
+                source.uri,
+                (originalWidth, originalHeight) => {
+                    if (!imagesMaxWidth) {
+                        this.setState({ width: originalWidth, height: originalHeight });
+                    }
+                    const optimalWidth = imagesMaxWidth <= originalWidth ? imagesMaxWidth : originalWidth;
+                    const optimalHeight = (optimalWidth * originalHeight) / originalWidth;
+                    this.setState({ width: optimalWidth, height: optimalHeight});
+                },
+                () => {
+                    this.setState({ width: -1,  height: -1 });
+                }
+            );
+        }
     }
 
-    componentWillUnmount () {
-        this.mounted = false;
+    componentDidMount () {
+        if (!this.state.width && !this.state.height) {
+            this.getImageSize();
+        }
     }
 
     componentDidUpdate(prevProps, prevState) {
-        this.getImageSize(this.props);
-    }
+        if (prevProps.source !== props.source || prevProps.width !== props.width ||
+            prevProps.height !== props.height || prevProps.style !== props.style) {
 
-    getDimensionsFromStyle (style, height, width) {
-        let styleWidth;
-        let styleHeight;
-
-        if (height) {
-            styleHeight = height;
+            this.getImageSize();
         }
-        if (width) {
-            styleWidth = width;
-        }
-        if (Array.isArray(style)) {
-            style.forEach((styles) => {
-                if (!width && styles['width']) {
-                    styleWidth = styles['width'];
-                }
-                if (!height && styles['height']) {
-                    styleHeight = styles['height'];
-                }
-            });
-        } else {
-            if (!width && style['width']) {
-                styleWidth = style['width'];
-            }
-            if (!height && style['height']) {
-                styleHeight = style['height'];
-            }
-        }
-
-        return { styleWidth, styleHeight };
-    }
-
-    getImageSize (props = this.props) {
-        const { source, imagesMaxWidth, style, height, width } = props;
-        const { styleWidth, styleHeight } = this.getDimensionsFromStyle(style, height, width);
-
-        if (styleWidth && styleHeight) {
-            return this.mounted && this.setState({
-                width: typeof styleWidth === 'string' && styleWidth.search('%') !== -1 ? styleWidth : parseInt(styleWidth, 10),
-                height: typeof styleHeight === 'string' && styleHeight.search('%') !== -1 ? styleHeight : parseInt(styleHeight, 10)
-            });
-        }
-        // Fetch image dimensions only if they aren't supplied or if with or height is missing
-        Image.getSize(
-            source.uri,
-            (originalWidth, originalHeight) => {
-                if (!imagesMaxWidth) {
-                    return this.mounted && this.setState({ width: originalWidth, height: originalHeight });
-                }
-                const optimalWidth = imagesMaxWidth <= originalWidth ? imagesMaxWidth : originalWidth;
-                const optimalHeight = (optimalWidth * originalHeight) / originalWidth;
-                this.mounted && this.setState({ width: optimalWidth, height: optimalHeight, error: false });
-            },
-            () => {
-                this.mounted && this.setState({ error: true });
-            }
-        );
-    }
-
-    validImage (source, style, props = {}) {
-        return (
-            <Image
-              source={source}
-              style={[style, { width: this.state.width, height: this.state.height, resizeMode: 'cover' }]}
-              {...props}
-            />
-        );
-    }
-
-    get errorImage () {
-        return (
-            <View style={{ width: 50, height: 50, borderWidth: 1, borderColor: 'lightgray', overflow: 'hidden', justifyContent: 'center' }}>
-                { this.props.alt ? <Text style={{ textAlign: 'center', fontStyle: 'italic' }}>{ this.props.alt }</Text> : false }
-            </View>
-        );
     }
 
     render () {
         const { source, style, passProps } = this.props;
+        const hasError = this.state.width < 0 || this.state.height < 0;
 
-        return !this.state.error ? this.validImage(source, style, passProps) : this.errorImage;
+        return !hasError ? (
+            <Image
+              source={source}
+              style={[style, { width: this.state.width, height: this.state.height, resizeMode: 'cover' }]}
+              {...passProps}
+            />
+        ) : (
+            <View style={{ width: 50, height: 50, borderWidth: 1, borderColor: 'lightgray', overflow: 'hidden', justifyContent: 'center' }}>
+                { this.props.alt ? <Text style={{ textAlign: 'center', fontStyle: 'italic' }}>{ this.props.alt }</Text> : false }
+            </View>
+        )
     }
 }
